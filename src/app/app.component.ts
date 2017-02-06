af0cf75ea922ceab54b9637b7f48ef3ce4998a22
Ray Artz <ray.e.artz@gmail.com>
2017-02-06 09:42:33 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  public dives = [
+    {
+      site: 'Abu Gotta Ramada',
+      location: 'Hurghada, Egypt',
+      depth: 72,
+      time: 54
+    },
+    {
+      site: 'Ponte Mahoon',
+      location: 'Maehbourg, Mauritius',
+      depth: 54,
+      time: 38
+    },
+    {
+      site: 'Molnar Cave',
+      location: 'Budapest, Hungary',
+      depth: 98,
+      time: 62
+    }];
+
 }
